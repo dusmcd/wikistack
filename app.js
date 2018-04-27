04a9890461f6f85e849a076486b565b52b95c987
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-04-27 10:13:30 -0500

    added styles and html

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
+const layout = require('./views/layout');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hey there!');
+  res.send(layout(''));
 });
 
 app.listen(8080, 'localhost', () => {
